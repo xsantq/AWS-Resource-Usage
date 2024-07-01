75a26ca471225e1e97ee56b2fb0a4536cafedf84
Cem <xsantq@hotmail.com>
2024-07-01 14:18:22 +0300

    script modified

--- aws-resource-usage.sh
+++ aws-resource-usage.sh
@@ -11,26 +11,39 @@
 ###################################
 
 
-#exec 2>&1>& aws-usage-report.txt 
 exec 1> >(tee -a "aws-usage-report.log")
 # List S3 buckets
 
-echo "print S3 Bucket List"
+
+echo "Creating EC2 instance"
+aws ec2 run-instances --image-id ami-04b70fa74e45c3917 --instance-type t2.micro --key-name aws_key_pair --security-group-ids sg-0d4d59af8d7e4111d
+echo "_____________________________________________"
+
+
+
+echo  "print S3 Bucket List"
 aws s3 ls 
+echo "_____________________________________________"
 
 # Describe instantance information
 
-echo "print instance description"
-aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId' 
+echo  "print instance description"
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+echo "_____________________________________________"
 
 # List Lambda Functions
 echo "print Lambda Functions"
 aws lambda list-functions 
-
+echo "_____________________________________________"
 # list AWS IAM users
 
 echo "print IAM user list"
 aws iam list-users  
+echo "_____________________________________________"
+#list VPC
+echo  "print VPC list"
+aws ec2 describe-vpcs
+echo "_____________________________________________"
 
-
-
+echo "List elatic IPs "
+aws ec2 describe-addresses
